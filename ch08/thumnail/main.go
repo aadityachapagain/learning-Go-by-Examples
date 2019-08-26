9a341867c191964b076a3ea99c5b30cf988919aa
AadityaChapagain <chapagain.aaditya@gmail.com>
2019-08-27 00:07:10 +0545

    add main program to run thumbnail library

---
+++ ch08/thumnail/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"src/ch8/thumbnail"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		thumb, err := thumbnail.ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
