f999bd64fb53ba08a193d7fcddb6cb107712b5ea
AadityaChapagain <chapagain.aaditya@gmail.com>
2019-09-10 08:24:24 +0545

    update result of memo_test

--- ch09/memo1/memo_test.go
+++ ch09/memo1/memo_test.go
@@ -3,7 +3,6 @@ package memo_test
 import (
 	"testing"
 
-	"gopl.io/ch9/memo1"
 	"gopl.io/ch9/memotest"
 )
 
@@ -20,21 +19,7 @@ func TestConcurrent(t *testing.T) {
 	memotest.Concurrent(t, m)
 }
 
-
 /*
 //!+output
 $ go test -v wikipedia.com
-=== RUN   Test
-https://golang.org, 175.026418ms, 7537 bytes
-https://godoc.org, 172.686825ms, 6878 bytes
-https://play.golang.org, 115.762377ms, 5767 bytes
-http://gopl.io, 749.887242ms, 2856 bytes
-https://golang.org, 721ns, 7537 bytes
-https://godoc.org, 152ns, 6878 bytes
-https://play.golang.org, 205ns, 5767 bytes
-http://gopl.io, 326ns, 2856 bytes
---- PASS: Test (1.21s)
-PASS
-ok  gopl.io/ch9/memo1	1.257s
-//!-output
 */
