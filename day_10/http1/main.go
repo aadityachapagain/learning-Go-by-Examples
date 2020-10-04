c3a72a7beb9b9833b303df042429339ca55c1aa8
aadityachapagain <chapagain.aaditya@gmail.com>
2020-10-04 20:17:42 +0545

    http with mux :go

---
+++ day_10/http1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
+
+type dollars float32
+
+func (d dollars) String() { return fmt.SprintF("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
