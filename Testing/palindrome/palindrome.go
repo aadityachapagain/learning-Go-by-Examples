715d7f468a20d74c8d9069ccc54de5d2435b3613
aadityachapagain <chapagain.aaditya@gmail.com>
2020-11-01 08:48:01 +0545

    palindrome testing example

---
+++ Testing/palindrome/palindrome.go
@@ -0,0 +1,21 @@
+package palindrome
+
+import "unicode"
+
+// Is Palindrome reports wheather s reads the same forward and backward.
+// Letter case is ignored, as are non-letters
+func IsPalindrome(s string) bool {
+  var letters []rune
+  for _, r := range s {
+    if unicode.IsLetter(r) {
+      letters = append(letters, unicode.ToLower(r))
+    }
+  }
+
+  for i := range letters {
+    if letters[i] != letters[len(letters)-1 - i] {
+      return false
+    }
+  }
+  return true
+}
---
+++ Testing/palindrome/palindrome_test.go
@@ -0,0 +1,50 @@
+package palindrome
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T){
+  var tests = []struct {
+    input string
+    want bool
+  }{
+    {"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // non-palindrome
+		{"desserts", false},   // semi-palindrome
+  }
+
+  for _, test := range tests {
+    if got := IsPalindrome(test.input); got != test.want {
+      t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+    }
+  }
+}
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
+
+func ExampleIsPalindrome() {
+	fmt.Println(IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(IsPalindrome("palindrome"))
+	// Output:
+	// true
+	// false
+}
