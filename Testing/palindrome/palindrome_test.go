e136c5f792a5f58a58a15e3c284d45074cc596c5
aaditya chapagain <chapagain.aaditya@gmail.com>
2020-12-02 10:26:36 +0545

    simpler palindrome test funcs

--- Testing/palindrome/palindrome_test.go
+++ Testing/palindrome/palindrome_test.go
@@ -6,6 +6,34 @@ import (
 
 import "testing"
 
+func TestPalindrome(t *testing.T) {
+  if !IsPalindrome("detartrated") {
+    t.Errorf(`IsPalindrome("detartrated") = false`)
+  }
+  if !IsPalindrome("kayak") {
+    t.Errorf(`IsPalindrome("kayak") = false`)
+  }
+}
+
+func TestNonPalindrome(t *testing.T) {
+  if IsPalindrome("palindrome") {
+    t.Error(`IsPalindrome("palindrome") = true`)
+  }
+}
+
+func TestFrenchPalindrome(t *testing.T) {
+  if !IsPalindrome("été") {
+    t.Error(`IsPalindrome("été") = false`)
+  }
+}
+
+func TestCanalPalindrome(t *testing.T) {
+  input := "A man, a plan, a canal: Panama"
+    if !IsPalindrome(input) {
+      t.Errorf(`IsPalindrome(%q) = false`, input)
+  }
+}
+
 func TestIsPalindrome(t *testing.T){
   var tests = []struct {
     input string
